28190e9131f2fcf4456683fc49625544547a745b
arunaborisovad15 <arunaborisovad@mail.ru>
2024-05-31 08:38:53 +0800

    new

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let button = document.querySelector("button");
 let input = document.querySelector(".input-in");
 let tasks = document.querySelector(".ul");
-let newTask = document.getElementById("li");
+
 let div = document.getElementById("div");
 
 
-button.onclick = function(){
+button.addEventListener("click", function(){
     let taskText = input.value;
+    let newTask = document.createElement("li");
     newTask.textContent = taskText;
 tasks.append(newTask);
 input.value = '';
 
-}
+})
